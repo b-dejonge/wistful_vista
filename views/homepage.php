5b0cc2d30c464c5aba0ffc979c92783792e09f8b
bowler229 <298.bowl@gmail.com>
2017-11-16 08:52:55 -0500

    Changed to responsive nav

--- views/homepage.php
+++ views/homepage.php
@@ -1,16 +1,70 @@
 <!-- <div class="hero">
 	<img class="hero-img" src="views/images/hero-img.jpg" alt="apartment view">
 </div> -->
-<div class="navbar">
+<!-- <div class="navbar">
 	<div class="nav-left">
 		<a href="https://placeholder.com"><img src="http://via.placeholder.com/250x50"></a>
 	</div>
 	<div class="nav-right">
-		<a href="#home">Home</a>
- 		<a href="#news">News</a>
-		<a href="#contact">Contact</a>
+		<a href="#">Home</a>
+		<a href="#">Apartments</a>
+ 		<a href="#">Take a Tour</a>
+		<a href="#">Login</a>
 	</div>
-</div>
+</div> -->
+<section class="navigation">
+  <div class="nav-container">
+    <div class="brand">
+      <a href="#!">Logo</a>
+    </div>
+    <nav>
+      <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+      <ul class="nav-list">
+        <li>
+          <a href="#!">Home</a>
+        </li>
+        <li>
+          <a href="#!">About</a>
+        </li>
+        <li>
+          <a href="#!">Services</a>
+          <ul class="nav-dropdown">
+            <li>
+              <a href="#!">Web Design</a>
+            </li>
+            <li>
+              <a href="#!">Web Development</a>
+            </li>
+            <li>
+              <a href="#!">Graphic Design</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#!">Pricing</a>
+        </li>
+        <li>
+          <a href="#!">Portfolio</a>
+          <ul class="nav-dropdown">
+            <li>
+              <a href="#!">Web Design</a>
+            </li>
+            <li>
+              <a href="#!">Web Development</a>
+            </li>
+            <li>
+              <a href="#!">Graphic Design</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#!">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</section>
+
 
 <div id="hero">
 	<div class="header">
@@ -18,5 +72,5 @@
 	</div>
 </div>
 <div class="main">
-	
+
 </div>
