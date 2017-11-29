208acd86223968d72ec6d42876a8ce8b2913296b
bowler229 <298.bowl@gmail.com>
2017-11-28 23:31:40 -0500

    Homepage edits

--- views/homepage.php
+++ views/homepage.php
@@ -24,8 +24,6 @@
     </div><!-- /.container -->
 </nav><!-- /.navbar -->
 
-
-
 <div id="hero">
   <div id="hero-overlay"></div>
   <div class="header">
@@ -39,8 +37,125 @@
 </div>
 
 <div class="main">
-
+  <div class="row">
+    <div class="col-xs-12 heading">
+      <h2>Featured Apartments</h2>
+    </div>
+    <p class="subheading">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+  </div>
+  <div class="row featured">
+    <div class="col-xs-12 gutter">
+      <div class="col-lg-4 col-xs-12">
+        <div class="featuredApt">
+          <img src="http://via.placeholder.com/350x233">
+          <div class="featuredApt-contents">
+            <div class="featuredApt-header clearfix">
+              <div class="pull-left">
+                <h5 class="entry-title">Affordable and Modern</h5>
+                <span class="view-floor-plan"><i class="fa fa-building-o" aria-hidden="true"></i> View floor plan</span>
+              </div>
+              <button class="btn btn-default pull-right" style="font-size:15px;">$389.99</button>
+            </div>
+            <div class="featuredApt-meta clearfix">
+              <span><i class="fa fa-arrows-alt"></i> 2150 SqFt</span>
+              <span><i class="fa fa-bed"></i> 3 Beds</span>
+              <span><i class="fa fa-bathtub"></i> 1.5 Baths</span>
+              <span><i class="fa fa-cab"></i> Yes</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-xs-12">
+        <div class="featuredApt">
+          <img src="http://via.placeholder.com/350x233">
+          <div class="featuredApt-contents">
+            <div class="featuredApt-header clearfix">
+              <div class="pull-left">
+                <h5 class="entry-title">Modern Family Style</h5>
+                <span class="view-floor-plan"><i class="fa fa-building-o" aria-hidden="true"></i> View floor plan</span>
+              </div>
+              <button class="btn btn-default pull-right" style="font-size:15px;">$389.99</button>
+            </div>
+            <div class="featuredApt-meta clearfix">
+              <span><i class="fa fa-arrows-alt"></i> 2840 SqFt</span>
+              <span><i class="fa fa-bed"></i> 4 Beds</span>
+              <span><i class="fa fa-bathtub"></i> 2.5 Baths</span>
+              <span><i class="fa fa-cab"></i> Yes</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-xs-12">
+        <div class="featuredApt">
+          <img src="http://via.placeholder.com/350x233">
+          <div class="featuredApt-contents">
+            <div class="featuredApt-header clearfix">
+              <div class="pull-left">
+                <h5 class="entry-title">Modern Studio</h5>
+                <span class="view-floor-plan"><i class="fa fa-building-o" aria-hidden="true"></i> View floor plan</span>
+              </div>
+              <button class="btn btn-default pull-right" style="font-size:15px;">$389.99</button>
+            </div>
+            <div class="featuredApt-meta clearfix">
+              <span><i class="fa fa-arrows-alt"></i> 1380 SqFt</span>
+              <span><i class="fa fa-bed"></i> 1 Beds</span>
+              <span><i class="fa fa-bathtub"></i> 1 Baths</span>
+              <span><i class="fa fa-cab"></i> Yes</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> <!-- End Main -->
+<div class="row announcement">
+  <div class="col-xs-12 text-center">
+    <div class="announcement-main">
+      <h2>Experience greatness for yourself</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+      <div class="buttons">
+        <a href="index.php?action=contact"><button class="btn btn-default">Take a Tour</button></a>
+        <a href="index.php?action=apartments"><button class="btn btn-default">View Apartments</button></a>
+      </div>
+    </div>
+  </div>
+</div> <!-- End Announcement -->
+<div class="why-us-bg">
+  <div class="row why-us">
+    <div class="col-xs-12">
+      <div class="row">
+        <div class="col-xs-12 heading">
+          <h2>Why Choose Us</h2>
+        </div>
+        <p class="subheading">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+      </div>
+      <div class="row why-us-contents text-center">
+        <div class="col-md-3 col-xs-6">
+          <img src="views/images/money-pin.png" width="90">
+          <h5 class="entry-title">Affordable</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div class="col-md-3 col-xs-6">
+          <i class="fa fa-building-o"></i>
+          <h5 class="entry-title">Affordable</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div class="col-md-3 col-xs-6">
+          <i class="fa fa-tree"></i>
+          <h5 class="entry-title">Affordable</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div class="col-md-3 col-xs-6">
+          <i class="fa fa-car"></i>
+          <h5 class="entry-title">Affordable</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+
+
 
 </body>
 
